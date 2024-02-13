414d09017d6b3672c6b0da29c413f638a3c45f17
florla <feliciamlbrownwork@gmail.com>
2024-02-13 08:53:00 -0500

    questionaire comments

--- frontend/src/pages/Questionaire.jsx
+++ frontend/src/pages/Questionaire.jsx
@@ -1,20 +1,15 @@
-import React from 'react'
-import QuizComponent from '../components/QuizComponent'
-import Evaluation from '../components/Evaluation'
-import NextButtonComponent from '../components/NextButton'
-import SingleQ from '../components/SingleQ'
-import { useLocation } from 'react-router-dom';
+import React from 'react'; 
+import QuizComponent from '../components/QuizComponent'; 
+import { useLocation } from 'react-router-dom'; 
 
-export default function Questionaire() {
-    const location = useLocation();
-    const results = location.state && location.state.results;
-    console.log(results);
+export default function Questionnaire() {
+    const location = useLocation(); // Use useLocation hook to access the current location
+    const results = location.state && location.state.results; // Extract the results from the location state
 
     return (
-        <div className='quizPage'>
+        <div>
+            {/* Render the QuizComponent with results */}
             <QuizComponent results={results} />
-            <Evaluation />
-            <NextButtonComponent />
         </div>
-    )
+    );
 }
