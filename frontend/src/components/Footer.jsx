803a3e5fa3baf1857dcbe2c68ed50eafdfe5865b
florla <feliciamlbrownwork@gmail.com>
2024-02-13 08:47:17 -0500

    footer comment

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Footer() {
+    // Rendering the Footer component
     return (
         <footer className="page-footer blue">
             <div className="container">
                 <div className="row">
+                    {/* Left column with app description */}
                     <div className="col l6 s12">
                         <p className="grey-text text-lighten-4">Embrace the power of our app and unlock the secrets of the universe, one quiz at a time. As I always say, 'Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.'</p>
-
-
                     </div>
+                    {/* Middle column, currently empty */}
                     <div className="col l3 s12"></div>
+                    {/* Right column with links */}
                     <div className="col l3 s12">
                         <p className="white-text header-5">Links</p>
                         <ul>
                             <li><Link className="white-text" to="/">Home</Link></li>
                             <li><Link className="white-text" to="/categories">Quiz Generation</Link></li>
                             <li><Link className="white-text" to="/account">Account</Link></li>
-                        
                         </ul>
                     </div>
-                    <div className="col l3 s12">
-                    </div>
+                    {/* Empty column */}
+                    <div className="col l3 s12"></div>
                 </div>
             </div>
-
-            
+            {/* Footer copyright */}
+            <div className="footer-copyright">
+                <div className="container">
+                    Made by <a className="white-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                </div>
+            </div>
         </footer>
     )
 }
